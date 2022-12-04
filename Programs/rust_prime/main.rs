cf1e284b05606f1693df900c2ce8ba219fbca598
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-12-04 12:36:44 +0100

    Update main.rs

--- Programs/rust_prime/main.rs
+++ Programs/rust_prime/main.rs
@@ -1,9 +1,7 @@
-use std::time::Instant;
+// This program was written with ChatGPT
 
 fn main() {
     let mut primes = Vec::new();
-
-    let start_time = Instant::now();
 
     // Start at 2, because 1 is not a prime number
     for n in 2..100_000 {
@@ -21,8 +19,4 @@ fn main() {
             primes.push(n);
         }
     }
-
-    let elapsed_time = start_time.elapsed();
-    let elapsed_ms = (elapsed_time.as_secs() * 1_000) + (elapsed_time.subsec_nanos() / 1_000_000) as u64;
-    println!("{}", elapsed_ms);
 }
